954344bf89c1d78f49de1da601aad02587825ad9
hackhub817 <piyushguptaji123@gmail.com>
2024-12-06 17:51:37 +0530

    cors fix

--- server/server.js
+++ server/server.js
@@ -33,6 +33,7 @@ app.use(
       "http://localhost:5174",
       "http://localhost:5175",
       process.env.FRONTEND_URL,
+      process.env.ADMIN_FRONTEND_URL,
     ],
     credentials: true,
   })
